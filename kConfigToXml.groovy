42678c7216aec0138854d410cccd24ad489b2232
Johannes Harth <166_MMX@gmx.net>
2014-03-13 08:48:21 +0100

    complete makeover

--- kConfigToXml.groovy
+++ kConfigToXml.groovy
@@ -3,22 +3,146 @@
 import groovy.xml.XmlUtil
 import org.apache.commons.lang3.StringEscapeUtils
 
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 @Grab(group='org.apache.commons', module='commons-lang3', version='3.2.1')
 
-enum ParserState {
-    INITIAL,
-    COMMAND,
-    PARAM,
-    STRING,
-    HELP,
-    EOL
+enum ExpressionType
+{
+    NONE,
+    OR,
+    AND,
+    NOT,
+    EQUAL,
+    UNEQUAL,
+    LIST,
+    SYMBOL,
+    RANGE
 }
 
-class KConfigToXml
+enum SymbolType
 {
+    UNKNOWN,
+    BOOLEAN,
+    TRI_STATE,
+    INT,
+    HEX,
+    STRING,
+    OTHER
+}
+
+enum PropertyType
+{
+    UNKNOWN,
+    PROMPT,   /* prompt "foo prompt" or "BAZ Value" */
+    COMMENT,  /* text associated with a comment */
+    MENU,     /* prompt associated with a menuconfig option */
+    DEFAULT,  /* default y */
+    CHOICE,   /* choice value */
+    SELECT,   /* select BAR */
+    RANGE,    /* range 7..100 (for a symbol) */
+    ENV,      /* value from environment variable */
+    SYMBOL    /* where a symbol is defined */
+}
+
+class HelpReader
+{
+    private static  def int            INIT_INDENT_LENGTH  = -1
+    private static  def Pattern        P_HELP_INDENT       = ~/^[ \t]+/
+
+    private         def int            firstLength         = INIT_INDENT_LENGTH
+    private         def StringBuilder  sb                  = null
+    private         def Node           parent              = null
+    private         def boolean        active              = false
+
+    def void start (Node parent)
+    {
+        reset()
+        active       = true
+        this.parent  = parent
+
+    }
+
+    def void reset ()
+    {
+        firstLength  = INIT_INDENT_LENGTH
+        sb           = new StringBuilder()
+        active       = false
+        parent       = null
+    }
+
+    def void finish ()
+    {
+        String helpText  = sb.toString().trim()
+        new Node(parent, 'help', null, helpText)
+        active           = false
+        reset()
+    }
+
+    def boolean read (String line)
+    {
+        boolean emptyBuilder = sb.size() == 0
+
+        if (!emptyBuilder && line.trim().empty)
+        {
+            sb << '\n'
+            return false
+        }
+
+        String indent = getIndent (line)
+        if (indent != null)
+        {
+            sb << indent
+            sb << line.replaceAll(P_HELP_INDENT, '')
+            sb << '\n'
+            return false
+        }
+
+        finish()
+        return true
+    }
+
+    private def String getIndent (String line)
+    {
+        int lineLength = line.length()
+        int indentLength = 0
+
+        for (int i = 0; i < lineLength; i++)
+            if (line[i] == '\t')
+                indentLength = (indentLength & ~7) + 8
+            else if (line[i] == ' ')
+                indentLength++
+            else
+                break
+
+        String indent = null
+        if (firstLength == INIT_INDENT_LENGTH)
+        {
+            firstLength = indentLength
+            indent = ''
+        }
+        else if (indentLength >= firstLength)
+        {
+            indentLength -= firstLength
+            indent = (' ' * indentLength)
+        }
+        indent
+    }
+
+    boolean getActive ()
+    {
+        return active
+    }
+}
+
+class LkcReader
+{
+//    static String T_EOL        = ''
+//    static String T_HELP_TEXT  = ''
+//    static String T_WORD_QUOTE  = ''
+//    static String T_WORD  = ''
+//    static String T_TYPE  = ''
+
     static String T_TRI_STATE  = 'tristate'
     static String T_STRING     = 'string'
     static String T_HEX        = 'hex'
@@ -46,6 +170,7 @@ class KConfigToXml
     static String T_COMMENT      = 'comment'
 
     static String T_DEPENDS      = 'depends'
+    static String T_ON           = 'on'
     static String T_SELECT       = 'select'
 
     static String T_PROMPT       = 'prompt'
@@ -74,105 +199,63 @@ class KConfigToXml
     static String R_EXPR                 = "(?:(?:$T_NOT\\s*)?$R_EXPR_SIMPLE|(?:(?:$T_NOT\\s*)?$R_EXPR_ENCLOSED)+)"
     static String R_EXPRESSION_NESTED    = "(?:$R_EXPR(?:\\s*(?:\\|\\||&&)\\s*$R_EXPR)*)"
     static Pattern P_SIMPLE_COMMENT = ~/^\s*#.*$|\s*#[^'"]*$/
-    static Pattern P_HELP_INDENT = ~/^[ \t]+/
     static Pattern P_COMMENT     = ~/$R_COMMENT/
     static Pattern P_WORD        = ~/$R_COMMAND_WORD|$R_PARAM_WORD/
     static Pattern P_WORD_QUOTE  = ~/$R_SINGLE_QUOTE_STRING|$R_DOUBLE_QUOTE_STRING/
     static Pattern TOKENIZER     = ~/$R_SINGLE_QUOTE_STRING|$R_DOUBLE_QUOTE_STRING|$R_EXPRESSION_NESTED/
 
-    static String getHelpIndent (String s, int firstLength)
+    static def boolean isCommonParent (Node parent)
     {
-        if (firstLength == -1)
+        def boolean result = false
+        def String name = parent.name()
+        switch (name)
         {
-            firstLength = 0
-        }
-        String indent = null
-        def sLength = s.length()
-        int indentLength = 0
-
-        for (int i = 0; i < sLength; i++)
-            if (s[i] == '\t')
-                indentLength = (indentLength & ~7) + 8
-            else if (s[i] == ' ')
-                indentLength++
-            else
-                break
-
-        if (indentLength >= firstLength)
-        {
-            indentLength -= firstLength
-            indent = (' ' * indentLength)
-        }
-        indent
-    }
-
-    static def boolean isSimpleContainerNode (Node parent)
-    {
-        switch (parent.name())
-        {
-            case T_HELP:
-            case T_CONFIG:
-            case T_COMMENT:
-                true
+            case T_MENU:
+            case T_CHOICE:
+            case T_IF:
+                result = true
                 break
             default:
-                false
                 break
         }
+        result
     }
 
     static def Node getParent (String command, Stack<Node> parents)
     {
-        Node parent = parents.peek()
+        def Node parent = parents.peek()
         switch (command)
         {
-            case T_BOOL:
-            case T_BOOLEAN:
-            case T_HEX:
-            case T_INT:
-            case T_STRING:
-            case T_TRI_STATE:
-                break
-
-            case T_DEFAULT_BOOLEAN:
-            case T_DEFAULT_TRI_STATE:
-            case T_DEFAULT:
-                break
-
             case T_COMMENT:
             case T_CONFIG:
             case T_MENU_CONFIG:
-            case T_MENU:
             case T_IF:
             case T_SOURCE:
+            case T_MENU:
             case T_CHOICE:
-                while (isSimpleContainerNode(parent))
+                while (!isCommonParent(parent))
                 {
                     parents.pop()
                     parent = parents.peek()
                 }
                 break
             case T_END_MENU:
-                while (!(parent.name() == T_MENU || parent.name() == T_MENU_CONFIG))
+                while (parent.name() != T_MENU)
+                {
                     parent = parents.pop()
+                }
                 break
             case T_END_CHOICE:
-                while (!(parent.name() == T_CHOICE))
+                while (parent.name() != T_CHOICE)
+                {
                     parent = parents.pop()
+                }
                 break
             case T_END_IF:
-                while (!(parent.name() == T_IF))
+                while (parent.name() != T_IF)
+                {
                     parent = parents.pop()
-                break
-
-            case T_HELP:
-            case T_HELP_BOLD:
-                break
-
-            case T_DEPENDS:
-            case T_SELECT:
-            case T_RANGE:
-            case T_PROMPT:
+                }
                 break
 
             default:
@@ -181,226 +264,179 @@ class KConfigToXml
         parent
     }
 
-    static def Map readIfExpr (List<String> tokens, Map attributes)
+    static def String getQuotedWord (String word)
     {
-        int indexOfIf = tokens.indexOf(T_IF)
-        if (indexOfIf != -1)
+        // sym_expand_string_value
+        String result = word
+        if (result ==~ P_WORD_QUOTE)
         {
-            if (attributes == null)
-            {
-                attributes = [:]
-            }
-            String expr = tokens[indexOfIf + 1]
-            attributes['if'] = expr
+            result = result[1..-2]
+            result = StringEscapeUtils.unescapeJava(result)
         }
-        attributes
+        result
     }
 
-    static def Node readPrompt (List<String> tokens, Node parent)
+    static def String getPrompt (List<String> tokens)
     {
-        String prompt = null
+        String result = null
         if (tokens.size() > 1)
         {
-            prompt = tokens[1]
+            result = getQuotedWord(tokens[1])
         }
-        if (prompt ==~ P_WORD_QUOTE)
-        {
-            prompt = prompt[1..-2]
-            prompt = StringEscapeUtils.unescapeJava(prompt)
-        }
-
-        Map attributes = null
-        attributes = readIfExpr(tokens, attributes)
-
-        new Node(parent, T_PROMPT, attributes, prompt)
+        result
     }
 
-    static def void read (File kConfig)
+    static def String getCondition (List<String> tokens)
     {
-        Node root = new Node(null, 'kconfig')
+        String result = null
+        int index = tokens.indexOf(T_IF)
+        if (index != -1)
+        {
+            result = tokens[index + 1]
+        }
+        result
+    }
+
+    static def Node read (File kConfig)
+    {
         Stack<Node> parents = new Stack<>()
+        def Node root = new Node(null, T_MENU)
         parents << root
 
-        ParserState   state             = ParserState.INITIAL
-        int           firstIndentLength = -1
-        StringBuilder helpStringBuilder = null
+        HelpReader helpReader = new HelpReader()
 
         kConfig.text.eachLine { String line, int count ->
-            if (state == ParserState.HELP)
+            if (helpReader.active)
             {
-                if (line.trim().empty)
+                helpReader.read(line)
+                if (helpReader.active)
                 {
-                    helpStringBuilder << '\n'
-                    return
-                }
-                String indent = getHelpIndent(line, firstIndentLength)
-                if (indent == null)
-                {
-                    Node parent = parents.pop()
-                    String helpString = helpStringBuilder
-                    parent.setValue(helpString.trim())
-
-                    state = ParserState.INITIAL
-                    helpStringBuilder = null
-                    firstIndentLength = -1
-                }
-                else
-                {
-                    if (firstIndentLength == -1)
-                    {
-                        firstIndentLength = indent.length()
-                        indent = ''
-                    }
-                    helpStringBuilder << indent
-                    helpStringBuilder << line.replaceAll(P_HELP_INDENT, '')
-                    helpStringBuilder << '\n'
                     return
                 }
             }
+
             line = line.replaceAll(P_SIMPLE_COMMENT, '')
             if (line.trim().empty)
             {
                 return
             }
+
             List<String> tokens = (line =~ TOKENIZER).collect {it.toString()}
             String command = tokens[0]
-            Node parent = getParent(command, parents)
-            switch (command) {
+
+            def Map   attr    = [:]
+            def Node  parent  = getParent(command, parents)
+            def Node  node
+            def String prompt     = getPrompt(tokens)
+            def String condition  = getCondition(tokens)
+            switch (command)
+            {
+                case T_HELP:
+                case T_HELP_BOLD:
+                    helpReader.start(parent)
+                    break
+
                 case T_BOOL:
                 case T_BOOLEAN:
                 case T_HEX:
                 case T_INT:
                 case T_STRING:
                 case T_TRI_STATE:
-                    String typeValue = tokens[0]
-                    if (typeValue == T_BOOLEAN)
-                    {
-                        typeValue = T_BOOL
-                    }
-
-                    Map typeAttributes = [
-                            value: typeValue
-                    ]
-
-                    Node typeNode = new Node(parent, 'type', typeAttributes)
-                    readPrompt(tokens, typeNode)
+                    if (prompt)
+                        attr.prompt      = prompt
+                    if (condition)
+                        attr.condition   = condition
+                    new Node(parent, command, attr)
                     break
 
                 case T_DEFAULT_BOOLEAN:
-                    String typeValue = T_BOOL
-                    String defaultExpr = tokens[1]
-
-                    new Node(parent, 'type', [value: typeValue])
-                    new Node(parent, T_DEFAULT, null, defaultExpr)
-                    break
                 case T_DEFAULT_TRI_STATE:
-                    String typeValue = T_TRI_STATE
-                    String defaultExpr = tokens[1]
-
-                    new Node(parent, 'type', [value: typeValue])
-                    new Node(parent, T_DEFAULT, null, defaultExpr)
-                    break
                 case T_DEFAULT:
-                    Map attributes = null
-                    String expr = tokens[1]
+                    attr.expression  = tokens[1]
+                    if (condition)
+                        attr.condition   = condition
+                    new Node(parent, command, attr)
+                    break
 
-                    attributes = readIfExpr(tokens, attributes)
-                    new Node(parent, T_DEFAULT, attributes, expr)
+                case T_SELECT:
+                    attr.symbol      = tokens[1]
+                    if (condition)
+                        attr.condition   = condition
+                    new Node(parent, command, attr)
+                    break
+
+                case T_RANGE:
+                    attr.from        = tokens[1]
+                    attr.to          = tokens[2]
+                    if (condition)
+                        attr.condition   = condition
+                    new Node(parent, command, attr)
+                    break
+
+                case T_PROMPT:
+                    attr.text        = getPrompt(tokens)
+                    if (condition)
+                        attr.condition   = condition
+                    new Node(parent, command, attr)
+                    break
+
+                case T_DEPENDS:
+                    attr.expression  = tokens[2]
+                    new Node(parent, command, attr)
+                    break
+
+                case T_SOURCE:
+                    attr.reference   = getQuotedWord(tokens[1])
+                    new Node(parent, command, attr)
                     break
 
                 case T_IF:
-                    Map attributes = null
-
-                    attributes = readIfExpr(tokens, attributes)
-                    Node ifNode = new Node(parent, T_IF, attributes)
-                    parents << ifNode
+                    if (condition)
+                        attr.condition   = condition
+                    node = new Node(parent, command, attr)
+                    parents << node
                     break
                 case T_END_IF:
                     break
                 case T_CHOICE:
-                    Node menuNode = new Node(parent, T_CHOICE)
-                    parents << menuNode
+                    node = new Node(parent, command, null)
+                    parents << node
                     break
                 case T_END_CHOICE:
                     break
                 case T_MENU:
-                    Node menuNode = new Node(parent, T_MENU)
-                    readPrompt(tokens, menuNode)
-                    parents << menuNode
+                    attr.prompt      = getPrompt(tokens)
+                    node = new Node(parent, command, attr)
+                    parents << node
                     break
                 case T_END_MENU:
                     break
 
                 case T_CONFIG:
-                    String symbol = tokens[1]
-                    Node configNode = new Node(parent, T_CONFIG, [symbol:symbol])
-                    parents << configNode
+                    attr.symbol      = tokens[1]
+                    node = new Node(parent, command, attr)
+                    parents << node
                     break
 
                 case T_MENU_CONFIG:
-                    String symbol = tokens[1]
-                    Node configNode = new Node(parent, 'menu_config', [symbol:symbol])
-                    parents << configNode
+                    attr.symbol      = tokens[1]
+                    node = new Node(parent, command, attr)
+                    parents << node
                     break
 
                 case T_COMMENT:
-                    Node commentNode = new Node(parent, T_COMMENT)
-                    readPrompt(tokens, commentNode)
-                    parents << commentNode
-                    break
-
-                case T_HELP:
-                case T_HELP_BOLD:
-                    Node helpNode = new Node(parent, T_HELP)
-                    parents << helpNode
-                    state = ParserState.HELP
-                    helpStringBuilder = new StringBuilder()
-                    firstIndentLength = -1
-                    break
-
-                case T_DEPENDS:
-                    String expr = tokens[2]
-                    new Node(parent, 'depends_on', null, expr)
-                    break
-
-                case T_SELECT:
-                    Map attributes = null
-                    String symbol = tokens[1]
-
-                    attributes = readIfExpr(tokens, attributes)
-                    new Node(parent, T_SELECT, attributes, symbol)
-                    break
-
-                case T_RANGE:
-                    Map attributes = [:]
-                    attributes['from'] = tokens[1]
-                    attributes['to'] = tokens[2]
-
-                    attributes = readIfExpr(tokens, attributes)
-                    new Node(parent, T_RANGE, attributes)
-                    break
-
-                case T_PROMPT:
-                    readPrompt(tokens, parent)
-                    break
-
-                case T_SOURCE:
-                    String path = tokens[1]
-                    if (path ==~ P_WORD_QUOTE)
-                    {
-                        path = path[1..-2]
-                        path = StringEscapeUtils.unescapeJava(path)
-                    }
-
-                    new Node(parent, T_SOURCE, null, path)
+                    attr.prompt      = getPrompt(tokens)
+                    node = new Node(parent, command, attr)
+                    parents << node
                     break
 
                 default:
+                    println "$count: $line"
                     break
             }
             return
         }
-        println XmlUtil.serialize(root)
+        root
     }
 }
 
@@ -410,4 +446,5 @@ if (!cacheFile.exists())
     def sourceUrl = 'https://raw.github.com/torvalds/linux/master/arch/x86/Kconfig'
     cacheFile.text = sourceUrl.toURL().text
 }
-KConfigToXml.read(cacheFile)
+def Node root = LkcReader.read(cacheFile)
+println XmlUtil.serialize(root)
