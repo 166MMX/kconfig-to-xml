468a45b402fe5ca4e087062876bb46b4b460fffb
Johannes Harth <166_MMX@gmx.net>
2014-03-21 21:30:14 +0100

    logically restructured code

--- kConfigToXml.groovy
+++ kConfigToXml.groovy
@@ -2,12 +2,11 @@
 
 import groovy.xml.XmlUtil
 
-def base = 'https://raw.github.com/torvalds/linux/master'
-
 def Queue<String> input   = new LinkedList<>()
 def Queue<String> output  = new LinkedList<>()
 
-def archList = ['alpha','arc','arm','arm64','avr32','blackfin','c6x','cris','frv','hexagon','ia64','m32r','m68k','metag','microblaze','mips','mn10300','openrisc','parisc','powerpc','s390','score','sh','sparc','tile','unicore32','x86','xtensa']
+def base      = 'https://raw.github.com/torvalds/linux/master'
+def archList  = ['alpha', 'arc', 'arm', 'arm64', 'avr32', 'blackfin', 'c6x', 'cris', 'frv', 'hexagon', 'ia64', 'm32r', 'm68k', 'metag', 'microblaze', 'mips', 'mn10300', 'openrisc', 'parisc', 'powerpc', 's390', 'score', 'sh', 'sparc', 'tile', 'unicore32', 'x86', 'xtensa']
 
 archList.each {
     input << "arch/$it/Kconfig"
@@ -15,23 +14,23 @@ archList.each {
 
 while (input.size() > 0)
 {
-    def currentFileName = input.pop()
+    def currentFileName  = input.pop()
     output << currentFileName
 
-    def simpleFileName = currentFileName.tr('/', '_')
-    def cacheFileName = 'test/' + simpleFileName + '.cache'
-    def cacheFile = new File(cacheFileName)
-    def xmlFileName = 'test/' + simpleFileName + '.xml'
-    def xmlFile = new File(xmlFileName)
+    def simpleFileName   = currentFileName.tr('/', '_')
+    def cacheFileName    = 'test/' + simpleFileName + '.cache'
+    def cacheFile        = new File(cacheFileName)
+    def xmlFileName      = 'test/' + simpleFileName + '.xml'
+    def xmlFile          = new File(xmlFileName)
 
     if (!cacheFile.exists())
     {
-        def sourceUrl = base + '/' + currentFileName
-        cacheFile.text = sourceUrl.toURL().text
+        def sourceUrl    = base + '/' + currentFileName
+        cacheFile.text   = sourceUrl.toURL().text
     }
 
-    def Node root = LkcReader.read(cacheFile)
-    xmlFile.text = XmlUtil.serialize(root)
+    def Node root        = LkcReader.read(cacheFile)
+    xmlFile.text         = XmlUtil.serialize(root)
 
     ((NodeList) root.breadthFirst()).getAt('source').each{ Node it ->
         def ref = it.attribute('reference')
